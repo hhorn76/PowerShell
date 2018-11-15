bc325c61e1dab408e64c7d78b0ebf58fd93c6bad
Heiko Horn <40029809+hhorn76@users.noreply.github.com>
2018-11-15 20:43:46 +0100

    Update IfStatement.ps1

--- YouTube/IfStatement.ps1
+++ YouTube/IfStatement.ps1
@@ -1,49 +1,41 @@
 ﻿#Comparison operaters and If conditions
 #Demonstrate -eq operator
-If (1+2 -eq 3)
-{
+If (1+2 -eq 3) {
     “one plus two equals three”
 }
 
 #Demonstrate -ne operator
-If (2 -ne 3)
-{
+If (2 -ne 3) {
     “two does not equal three”
 }
 
 #Demonstrate -lt operator
-If (2 -lt 3)
-{
+If (2 -lt 3) {
     “two is less that three”
 }
 
 #Demonstrate -gt operator
-If (100 -gt 5)
-{
+If (100 -gt 5) {
     “one hundred is greater that five”
 }
 
 #Demonstrate -ge operator
-If (5 -ge 5)
-{
+If (5 -ge 5) {
     “ten is greater or equal to five”
 }
 
 #Demonstrate -le operator
-If (1 -le 7)
-{
+If (1 -le 7) {
     “one is less or eqal to seven”
 }
 
 #Demonstrate -ne operator
-If (6 -ne 7)
-{
+If (6 -ne 7) {
     “six is not eqal to seven”
 }
 
 #Demonstrate -like operator
-If ( "testosterone" -like "test*" )
-{
+If ( "testosterone" -like "test*" ) {
     "The wildcard string ""test*"" is like testosterone"
 }
 
@@ -63,8 +55,7 @@ $objDays -NotMatch "sun"
 #Demonstrate -contains operator
 $objTest="this", "is", "a", "test"
 $objTest.GetType().Name
-If ($objTest -contains "test")
-{
+If ($objTest -contains "test") {
     "Object variable contains the string test"
 }
 
@@ -94,44 +85,34 @@ $strDateTime -as [DateTime]
 "TEST" -ceq "test"
 
 #Demonstrate If Not
-If (-Not (1 -eq 2))
-{
+If (-Not (1 -eq 2)) {
     “Do something”
 }
-If (!(1 -eq 2))
-{
+If (!(1 -eq 2)) {
     "Do something awesome"
 }
 
 #Demonstarate -And operator
-If (!(1 -eq 2) -And (1 -ne 2)) 
-{
+If (!(1 -eq 2) -And (1 -ne 2))  {
     “Do something even more awesome"
 }
 
 #Demonstarate -Or operator
-If ((1 -eq 1) -Or (1 -ne 2)) 
-{
+If ((1 -eq 1) -Or (1 -ne 2))  {
     “Do something phenomenal"
 }
 
 #Demonstrate Else
-If (1 -eq 2)
-{
+If (1 -eq 2) {
     “True”
-}
-Else
-{
+} Else {
     “False”
 }
 
 #Demonstrate ElseIf
-If (1 -eq 2)
-{
+If (1 -eq 2) {
     “one eqals to two”
 }
-ElseIf (2 -eq 2)
-{
+ElseIf (2 -eq 2) {
     “two equals to two”
 }
-
