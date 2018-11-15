08a25088c08477e4d6076b90afe1ac07b5e1aad2
Heiko Horn <40029809+hhorn76@users.noreply.github.com>
2018-11-15 20:35:41 +0100

    Update ActiveDirectoryGroups.ps1

--- YouTube/ActiveDirectoryGroups.ps1
+++ YouTube/ActiveDirectoryGroups.ps1
@@ -4,16 +4,12 @@ $objUser=Get-ADUser -Identity hhorn
 $arrGroups="test0","test1","test2","test3"
 
 #Demonstrate group membership and group creation
-ForEach($group in $arrGroups)
-{
+ForEach($group in $arrGroups) {
     $objGroup=Get-ADGroup -Identity $group
-    IF($objGroup)
-    {
+    IF($objGroup) {
         Write-Host "Group does EXISTS" -ForegroundColor Yellow
         Add-ADGroupMember -Identity $group -Members $objUser.SamAccountName
-    }
-    Else
-    {
+    } Else {
         Write-Host "Group does not exist" -ForegroundColor Magenta
         New-ADGroup -Name $group -samAccountName $group.ToLower() -GroupCategory Security -GroupScope Universal -DisplayName $group
         Add-ADGroupMember -Identity $group -Members $objUser.SamAccountName
@@ -24,7 +20,6 @@ ForEach($group in $arrGroups)
 Remove-ADGroupMember -Identity "test0" -Members $objUser.SamAccountName -Confirm:$false
 
 #Demonstrate deleting AD groups
-ForEach($group in $arrGroups)
-{
+ForEach($group in $arrGroups) {
     Remove-ADGroup -Identity $group -Confirm:$false
 }
