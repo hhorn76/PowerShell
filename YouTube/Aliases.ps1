539e8e4541ba97e8d25976f112ca1e33af224a0f
Heiko Horn <40029809+hhorn76@users.noreply.github.com>
2018-11-15 20:32:29 +0100

    Add files via upload

---
+++ YouTube/Aliases.ps1
@@ -0,0 +1,6 @@
+﻿Get-Alias -Definition ForEach-Object
+Get-Alias -Definition Where-Object
+Get-Alias -Definition Get-Process
+Get-Alias -Definition Get-Alias
+gal -Name f*
+gal | ? {$_.Version -ne $null}
