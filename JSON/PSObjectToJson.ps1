0a3a10a3caf4d1ff8506f6294ef66146b13ee978
Heiko Horn <40029809+hhorn76@users.noreply.github.com>
2018-12-04 20:20:03 +0100

    Update PSObjectToJson.ps1

--- JSON/PSObjectToJson.ps1
+++ JSON/PSObjectToJson.ps1
@@ -1,1 +1,16 @@
+#Create variables
+$var1 = "10"
+$var2 = "above"
 
+#Create a new object
+$objUser=New-Object -TypeName PSObject
+
+#Add property values to the object
+Add-Member -InputObject $objUser -MemberType NoteProperty -Name duration -Value "$var1"
+Add-Member -InputObject $objUser -MemberType NoteProperty -Name operator -Value "$var2"
+Add-Member -InputObject $objUser -MemberType NoteProperty -Name priority -Value "critical" 
+Add-Member -InputObject $objUser -MemberType NoteProperty -Name threshold -Value "1" 
+Add-Member -InputObject $objUser -MemberType NoteProperty -Name time_function -Value "all" 
+
+#Convert the object to JSON
+$objUser | ConvertTo-Json
