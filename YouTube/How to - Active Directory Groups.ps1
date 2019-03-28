04b8ed7dd77c9707f2e6bbf46faee0b36bc91d69
Heiko Horn <hhorn@macOS-1013.mis-munich.de>
2019-03-28 18:55:20 +0100

    Create How to - Active Directory Groups.ps1

---
+++ YouTube/How to - Active Directory Groups.ps1
@@ -0,0 +1,25 @@
+﻿#Set variables
+$ErrorActionPreference="SilentlyContinue"
+$objUser=Get-ADUser -Identity hhorn 
+$arrGroups="test0","test1","test2","test3"
+
+#Demonstrate group membership and group creation
+ForEach($group in $arrGroups) {
+    $objGroup=Get-ADGroup -Identity $group
+    IF($objGroup) {
+        Write-Host "Group does EXISTS" -ForegroundColor Yellow
+        Add-ADGroupMember -Identity $group -Members $objUser.SamAccountName
+    } Else {
+        Write-Host "Group does not exist" -ForegroundColor Magenta
+        New-ADGroup -Name $group -samAccountName $group.ToLower() -GroupCategory Security -GroupScope Universal -DisplayName $group
+        Add-ADGroupMember -Identity $group -Members $objUser.SamAccountName
+    }
+}
+
+#Demonstrate removing AD users from group
+Remove-ADGroupMember -Identity "test0" -Members $objUser.SamAccountName -Confirm:$false
+
+#Demonstrate deleting AD groups
+ForEach($group in $arrGroups) {
+    Remove-ADGroup -Identity $group -Confirm:$false
+}
