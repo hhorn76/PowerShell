538535b6e70a551d7bdba97a8c689d7f3fd8f251
Heiko Horn <hhorn76@gmail.com>
2019-08-30 14:44:05 +0200

    Create EncodingAndDecodingBase64String.ps1

---
+++ Functions/EncodingAndDecodingBase64String.ps1
@@ -0,0 +1,16 @@
+﻿#Function to use Base64 to encoding a sring 
+Function encodePassword ($strPassword) {
+    $strEncoded = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($strPassword))
+    Write-Host $strEncoded -ForegroundColor Yellow
+}
+
+
+#Base64 deencoding 
+Function decodePassword ($strPassword) {
+    $strDecoded = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($strPassword))
+    Write-Host $strDecoded -ForegroundColor Yellow
+}
+
+#
+$strEncoded = encodePassword 'ThisIsMySecrepPassword'
+decodePassword $strEncoded
