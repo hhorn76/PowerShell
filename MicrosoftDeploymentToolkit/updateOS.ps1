e809e72a9d491d9d420ebd0b49a6021ead93ff87
Heiko Horn <hhorn76@gmail.com>
2019-11-06 14:56:14 +0100

    Update updateOS.ps1

--- MicrosoftDeploymentToolkit/updateOS.ps1
+++ MicrosoftDeploymentToolkit/updateOS.ps1
@@ -6,32 +6,42 @@
 Import-Module .\writeToLog.psm1
 Start-Log -FilePath "$LogPath\BDD.log"
 Write-Log -Message '################'
-Write-Log -Message 'Start WSUS service and update OS. '
+Write-Log -Message 'Start WSUS service and update OS.'
 Write-Log -Message '################'
 
-# Stop the WSUS service if running and disable it
-$serviceWsus = Get-Service -Name wuauserv
-if ($serviceWsus.StartType -eq 'Disabled') {
-    Write-Log -Message 'Enabling WSUS service.'
-    Set-Service -Name $serviceWsus -StartupType Manual
-} else {
-    Write-Log -Message 'WSUS service is already enabled.'
-}
-
+# Enable the WSUS service if disabled
 try {
-    Write-Log -Message "Installing NuGet Package."
-    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+    if ((Get-Service -Name wuauserv).StartType -eq 'Disabled') {
+        Write-Log -Message 'Enabling WSUS service.'
+        Set-Service -Name $serviceWsus -StartupType Manual
+    } else {
+        Write-Log -Message 'WSUS service is already enabled.'
+    }
 } catch {
     Write-Log -Message $_.Exception.Message -LogLevel 3
 }
 
+# Install NuGet Package with minimum version 2.8.5.201
 try {
-    Write-Log -Message "Installing PSWindowsUpdate Module."
-    Install-Module PSWindowsUpdate -AllowClobber -Force
+    if (!(Get-PackageProvider -Name NuGet)) {
+        Write-Log -Message "Installing NuGet Package."
+        Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+    }
 } catch {
     Write-Log -Message $_.Exception.Message -LogLevel 3
 }
 
+# Install PSWindowsUpdate Module
+try {
+    if (!(Get-Command -Name Install-WindowsUpdate)) {
+        Write-Log -Message "Installing PSWindowsUpdate Module."
+        Install-Module PSWindowsUpdate -AllowClobber -Force
+    }
+} catch {
+    Write-Log -Message $_.Exception.Message -LogLevel 3
+}
+
+# Accept and install Windowe and Microsoft Updates
 try {
     Write-Log -Message "Installing Microsoft Updates."
     Install-WindowsUpdate -AcceptAll -Install -MicrosoftUpdate -AutoReboot
