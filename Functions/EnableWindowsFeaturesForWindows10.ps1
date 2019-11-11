8c5cc0fda671253e019066d4845b4ed55f522b8b
Heiko Horn <hhorn76@gmail.com>
2019-11-11 12:23:07 +0100

    Create EnableWindowsFeaturesForWindows10.ps1

---
+++ Functions/EnableWindowsFeaturesForWindows10.ps1
@@ -0,0 +1,42 @@
+﻿### Enale Windows Optional Features on Windows 10
+### This script needs Administrative privileges
+
+# Function to enable a Windows optional feature
+function enableFeature ($strFeature) {
+    $objState = Get-WindowsOptionalFeature -Online | Where featurename -Like $strFeature
+    if ($objState.State -eq 'Disabled') {
+        Write-Host "Enabeling Windows Optional Feature: $strFeature." -ForegroundColor Yellow
+        Enable-WindowsOptionalFeature -Online -FeatureName $strFeature
+    } else {
+        Write-Host "Windows Optional Feature: $strFeature is already enabled."
+    }
+}
+
+# Function to enable a Windows optional feature
+function disableFeature ($strFeature) {
+    $objState = Get-WindowsOptionalFeature -Online | Where featurename -Like $strFeature
+    if ($objState.State -eq 'Enabled') {
+        Write-Host "Enabeling Windows Optional Feature: $strFeature." -ForegroundColor Yellow
+        Disable-WindowsOptionalFeature -Online -FeatureName $strFeature
+    } else {
+        Write-Host "Windows Optional Feature: $strFeature is already enabled."
+    }
+}
+
+# Enable NET-Framework-Core Windows Feature.
+$strFeature = 'NetFx3'
+enableFeature $strFeature
+# Disable NET-Framework-Core Windows Feature.
+disableFeature $strFeature
+
+# Enable ASP.NET Windows Feature.
+$strFeature = 'NetFx4Extended-ASPNET45'
+enableFeature $strFeature
+# Disable ASP.NET Windows Feature.
+disableFeature $strFeature
+
+# Enable .NET TCP Activation Windows Feature 
+$strFeature = 'WCF-TCP-Activation45'
+enableFeature $strFeature
+# Disable ASP.NET Windows Feature.
+disableFeature $strFeature
